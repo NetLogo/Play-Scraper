c21d82c6324998e7d1b700df49431b62ad1d083d
Robert Grider <robert.grider@northwestern.edu>
2015-03-23 12:21:04 -0500

    Loading a server, with a bootstrap script, not working as intended

--- src/main/scala/Scraper.scala
+++ src/main/scala/Scraper.scala
@@ -1,33 +1,86 @@
 package org.nlogo
 
-import play.{ Play, PlayRunHook }
+import java.io.File
 
-import sbt.{ AutoPlugin, taskKey }
+import sbt.{ AutoPlugin, taskKey, Runtime }
+import sbt.Keys._
 
+import play.Play
+import play.api.Mode
+import play.api.mvc.RequestHeader
+import play.api.mvc.EssentialAction
+import play.api.libs.iteratee.Iteratee
+import play.api.DefaultApplication
+import play.core.classloader.{ ApplicationClassLoaderProvider, DelegatingClassLoader }
+
+import scala.util.Success
+import scala.util.Random
 import scala.collection.JavaConversions._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import StartServer.simpleGetRequest
 
 object Scraper extends AutoPlugin {
-
   object autoImport {
-    val scrapeKey = taskKey[Unit]("scrape play")
-    val scrapeHooks = taskKey[Seq[PlayRunHook]]("scrape play")
+    val scrapePlay = taskKey[Unit]("scrape play")
   }
 
   import autoImport._
 
+  class ScraperDelegatingClassLoader(
+    playLoader: ClassLoader,
+    buildLoader: ClassLoader,
+    loaderProvider: ApplicationClassLoaderProvider)
+  extends DelegatingClassLoader(playLoader, buildLoader, loaderProvider) {
+    val scraperClassNames: Seq[String] = Seq(
+      "play.api.Application",
+      "org.nlogo.StartServer"
+    )
+
+    override def loadClass(name: String): Class[_] = {
+      println(name)
+      if (scraperClassNames.contains(name))
+        buildLoader.loadClass(name)
+      else
+        super.loadClass(name)
+    }
+  }
+
+  val buildLoader = Scraper.getClass.getClassLoader
+  val scraperLocation = Scraper.getClass.getProtectionDomain.getCodeSource.getLocation
+
   override val projectSettings = Seq(
-    scrapeHooks := {
-      Seq(PlayRunHook.makeRunHookFromOnStarted { sock => println("I'm here!") })
-    },
-    scrapeKey := {
-      import play.PlayImport.PlayKeys.playRunHooks
+    scrapePlay := {
       import Play._
-      Play.playRunTask(
-        scrapeHooks,
-        playDependencyClasspath,
-        playDependencyClassLoader,
-        playReloaderClasspath,
-        playReloaderClassLoader,
-        playAssetsClassLoader).toTask("").value
+      val commonClassLoader = playCommonClassloader.value
+      var appLoader: Option[ClassLoader] = None
+      val delegatingLoader = new ScraperDelegatingClassLoader(commonClassLoader, buildLoader, new ApplicationClassLoaderProvider {
+        def get = {
+          playReloaderClassLoader.value("reloader", (playReloaderClasspath.value.files.map(_.toURI.toURL) :+ scraperLocation).toArray, appLoader.get)
+        }
+      })
+    val loader = playDependencyClassLoader.value("PlayDependencyClassLoader", (playDependencyClasspath.value.files.map(_.toURI.toURL) :+ scraperLocation).toArray, delegatingLoader)
+
+    val assetLoader = playAssetsClassLoader.value(loader)
+    appLoader = Some(assetLoader)
+
+    val serverStarter = loader.loadClass("org.nlogo.StartServer$")
+    println(serverStarter.getFields.foreach((f: java.lang.reflect.Field) => println(f.toString)))
+    val ssInstance = serverStarter.getFields.head.get(null)
+    val ssApply = serverStarter.getDeclaredMethod("apply", classOf[java.io.File], classOf[ClassLoader])
+    ssApply.invoke(ssInstance, baseDirectory.value, assetLoader)
+      /*
+      val defaultAppConstructor = loader.loadClass("play.api.DefaultApplication").getDeclaredConstructors.head
+      val mode = loader.loadClass("play.api.Mode")
+      val devMode = mode.getMethod("Dev").invoke(mode)
+      val app = defaultAppConstructor.newInstance(baseDirectory.value, assetLoader, null, devMode).asInstanceOf[play.api.Application]
+      val rts = app.routes.get
+      val indexRequest = simpleGetRequest("/")
+      val action = rts.routes(indexRequest).asInstanceOf[EssentialAction]
+      val consumer = Iteratee.getChunks[Array[Byte]]
+      action(indexRequest).run.onComplete {
+        res => println(res)
+      }
+      */
     })
 }
---
+++ src/main/scala/StartServer.scala
@@ -0,0 +1,49 @@
+package org.nlogo
+
+import play.api.Mode
+import play.api.mvc.RequestHeader
+import play.api.mvc.EssentialAction
+import play.api.libs.iteratee.Iteratee
+import play.api.DefaultApplication
+import play.core.classloader.{ ApplicationClassLoaderProvider, DelegatingClassLoader }
+
+import scala.util.Success
+import scala.util.Random
+import scala.collection.JavaConversions._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object StartServer {
+  def simpleGetRequest(_path: String): RequestHeader = {
+    new RequestHeader {
+      def headers = new play.api.mvc.Headers {
+        val data = Seq()
+      }
+      def id = Random.nextInt
+      def method = "GET"
+      def path = _path
+      def queryString = Map[String, Seq[String]]()
+      def remoteAddress = "0.0.0.0"
+      def secure = false
+      def tags = Map[String, String]()
+      def uri = _path
+      def version = "HTTP/1.1"
+    }
+  }
+
+  def apply(baseDirectory: java.io.File, loader: ClassLoader): Unit = {
+    val mode = loader.loadClass("play.api.Mode")
+    val devMode = mode.getMethod("Dev").invoke(mode)
+    val app = new DefaultApplication(baseDirectory, loader, null, play.api.Mode.Test).asInstanceOf[play.api.Application]
+    println(app)
+    println(app.configuration.getString("application.router"))
+    println(app.routes)
+    val indexRequest = simpleGetRequest("/")
+    /*
+    val action = rts.routes(indexRequest).asInstanceOf[EssentialAction]
+    val consumer = Iteratee.getChunks[Array[Byte]]
+    action(indexRequest).run.onComplete {
+      res => println(res)
+    }
+    */
+  }
+}
